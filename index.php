b2984381ae4c9dd1f3b7a7228d05213ae25da993
Kim Cooperrider <kim.cooperrider@gmail.com>
2012-05-22 12:43:10 -0400

    colors

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <? include('include/home_header.php'); ?>
 
 <div id="container" style="overflow:hidden">
-
-		<div id="home_text">
 		
 			<div class="text" style="width: 600px">
 					<div class="bubble" id="about_bubble"><h2>About</h2></div>
@@ -13,11 +11,5 @@
 					<div class="bubble" id="updates_bubble"><h2>Events</h2></div>
 					<?= grab('index_events'); ?>
 			</div>
-			
-		</div>
-		
-		<div id="home_img">
-			<img src='../images/kids_img.png'/>
-		</div>
 
 <? include('include/footer.php'); ?>
